c9dd2f8dd1af95df99d20050c9af853e5e50f29f
OmargamalH <omargamalhamedb8@gmail.com>
2026-08-17 05:31:34 +0300

    edited the data source request

--- dags/Utilities.py
+++ dags/Utilities.py
@@ -5,16 +5,17 @@ import sqlalchemy as sql
 import logging as l
 import random
 
-api_url = "https://api.open-meteo.com/v1/forecast"
+
+meteo_url = "https://api.open-meteo.com/v1/forecast"
+
 logger = l.getLogger(__name__)
 l.basicConfig(format = '%(levelname)s - %(name)s - %(message)s' , level = l.DEBUG)
 database_url = "postgresql+psycopg2://postgres:admin@postgres_storage:5432/weather_db"
 engine = sql.create_engine(database_url)
 con = engine.connect()
 table = 'weather_data'
+extracted_data_path = "./extracted_data.csv"
 intermediate_data_path = "./intermediate_data.csv"
-
-
 
 def extract():
     try: 
@@ -23,19 +24,40 @@ def extract():
 
         logger.info("Extraction of data from API starts...")
 
-        params = {'latitude' : latitude , 'longitude' : longitude , 'hourly' : 'temperature_2m'}
-        results = r.get(api_url , params = params).json()
+        meteo_params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "current": ["temperature_2m", "relative_humidity_2m", "is_day", "precipitation", "rain", "showers", "snowfall", "pressure_msl", "surface_pressure", "cloud_cover", "weather_code", "wind_gusts_10m", "wind_direction_10m", "wind_speed_10m", "apparent_temperature"],
+        }
 
-        results_formated = {'latitude' : results['latitude'] , 'longitude' : results['longitude'] , 'time' : results['hourly']['time'] , 'temperature' : results['hourly']['temperature_2m']}
-        results_df = pd.DataFrame(results_formated)
+        
+        meteo_results = dict(r.get(meteo_url , params = meteo_params).json())
+        meteo_results.pop('current_units')
+        meteo_results_df = pd.json_normalize(meteo_results)
+        meteo_results_df.columns = list(map(lambda column: column.replace('current.' , '') , list(meteo_results_df.columns)))
 
+        meteo_results_df.to_csv(extracted_data_path, index = False)
+        
         logger.info("Extraction of data from API Ends.")
 
-        results_df.to_csv(intermediate_data_path , index = False)
-        return intermediate_data_path
+        return extracted_data_path
     except Exception as e:
         l.error(f"Error in extraction of data ({e})")
         raise Exception
+
+def transform():
+    try:
+        logger.info("Transforamtion of data starts...")
+        results = pd.read_csv(extracted_data_path)
+
+        results.is_day = results.is_day.astype(dtype = 'boolean')
+
+        results.to_csv(intermediate_data_path , index = False)
+        logger.info("Transforamtion of data ends...")
+    except Exception as e:
+        l.error(f"Error in transforamtion of data ({e})")
+        raise Exception
+
 
 
 def load():
@@ -54,10 +76,10 @@ def load():
 def remove_file():
     try:
         os.remove(intermediate_data_path)
+        os.remove(extracted_data_path)
         logger.info(f"intermediate data file has been removed from {intermediate_data_path}")
     except Exception as e:
         logger.error(f"Error in removing intermediate data file: {e}")
-
 
 
 
--- dags/pipeline.py
+++ dags/pipeline.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 
 from datetime import datetime
-from Utilities import extract , load , remove_file
+from Utilities import extract , transform , load , remove_file
 
 
 
@@ -10,8 +10,10 @@ with DAG("Weather_pipeline" , start_date = datetime(2026, 8, 16) , schedule = "*
 
     extract_data_task = PythonOperator(task_id = "extract_data_from_source" , python_callable= extract)
 
+    transform_data_task = PythonOperator(task_id = "transform_data" , python_callable= transform)
+
     load_data_task = PythonOperator(task_id = "load_data_to_destination" , python_callable= load)
 
     remove_intermediate_data = PythonOperator(task_id = "remove_intermediate_data" , python_callable= remove_file)
 
-    extract_data_task >> load_data_task >> remove_intermediate_data
+    extract_data_task >>  transform_data_task >> load_data_task >> remove_intermediate_data
